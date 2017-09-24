74fcf263af1fa30d9390c1e7da14e470bc9b391a
Daniel Elps <daniel.j.elps@gmail.com>
2017-09-24 16:07:41 -0400

    Added a skeleton that walks forward.

---
+++ Assets/Scripts/AnimControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+
+    private Animator mAnimator;
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mAnimator = GetComponent<Animator>();
+	    mAnimator.SetBool("walking",true);
+    }
+
+    // Update is called once per frame
+    void Update ()
+	{
+	    transform.position += transform.forward * Time.deltaTime * movementSpeed;
+
+
+        if (Input.GetKeyDown(KeyCode.M))
+	    {
+	        mAnimator.SetTrigger("attack");
+	    }
+	}
+}
